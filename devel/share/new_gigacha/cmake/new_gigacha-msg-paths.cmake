6057b3e6943135abb67da889c9c35496e349613b
sh <shs@inha.edu>
2021-11-12 16:44:31 +0900

    set msgs

---
+++ devel/share/new_gigacha/cmake/new_gigacha-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(new_gigacha_MSG_INCLUDE_DIRS "/home/cvlab/gigacha/planning_ws/src/new_gigacha/msg")
+set(new_gigacha_MSG_DEPENDENCIES geometry_msgs;nav_msgs;actionlib_msgs;new_gigacha)
