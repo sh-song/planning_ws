4bbf359e663e769f8f7778d9398cddff2c61f3a9
sh <shs@inha.edu>
2021-11-13 04:48:48 +0900

    path planner 틀 완성

---
+++ src/new_gigacha/scripts/lib/planner_utils/path_planner.py
@@ -0,0 +1,17 @@
+from new_gigacha.srv import *
+import rospy
+
+class PathPlanner:
+    def __init__(self, ego, where, name):
+        self.ego = ego
+        self.ego.global_path \
+            = self.read_global_path_from_map(where, name)
+
+    def read_global_path_from_map(self, where, name):
+        rospy.wait_for_service('global_map')
+        try:
+            global_map = rospy.ServiceProxy('global_map', GlobalMap)
+            return global_map(where, name).path
+
+        except rospy.ServiceException as e:
+                print(f"Service call failed: {e}")
--- src/new_gigacha/scripts/planner.py
+++ src/new_gigacha/scripts/planner.py
@@ -1,12 +1,11 @@
 from lib.general_utils.sig_int_handler import Activate_Signal_Interrupt_Handler
-
 from lib.planner_utils.global_path_planner import global_map_client
-from new_gigacha.msg import Local
 # from lib.planner_utils.mission_planner import MissionPlanner
-# from lib.planner_utils.path_planner import PathPlanner
+from lib.planner_utils.path_planner import PathPlanner
 # from lib.planner_utils.index_finder import IndexFinder
 from lib.planner_utils.sensor_hub import SensorHub
 
+from new_gigacha.msg import Local, Path
 import rospy
 
 class EgoVehicle:
@@ -14,15 +13,13 @@ class EgoVehicle:
         self.mission = ""
         self.state =""
         self.pose = Local() #x, y, heading
-        self.index = None
-
-        self.speed = None
-        self.brake = None
-        self.steer = None
-
+        self.index = -1
+        self.speed = -1
+        self.brake = -1
+        self.steer = -1
+        self.nearby_obs = []
+        self.global_path = Path()
     
-
-
 
 
 class Planner:
@@ -30,16 +27,16 @@ class Planner:
         rospy.init_node("Planner", anonymous=False)
         self.ego = EgoVehicle()
         self.sensor_hub = SensorHub(self.ego)
-        # self.mission_planner = MissionPlanner(self.ego)
-        # self.path_planner = PathPlanner(self.ego)
+        self.path_planner = PathPlanner(self.ego, 'kcity', 'final')
         # self.whereami = IndexFinder(self.ego)
+        # self.mission_planner = MissionPlanner(self.ego)
       
 
     def run(self):
         # self.mission_planner.run()
         # self.path_planner.run()
         # self.whereami.run()
-        print(self.ego.pose)
+        print(self.ego.global_path)
 
 
 # global_map = global_map_client("kcity", "final")
